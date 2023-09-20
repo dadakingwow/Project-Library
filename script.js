d60875135721e8132fb46598a8291d2b2756d3a6
dada <pftrew@gmail.com>
2023-09-19 20:17:44 -0400

    redo using class

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
-const modal = document.querySelector(".modal");
-const btn = document.querySelector(".addButton");
-const body = document.querySelector("body");
-
-btn.addEventListener("click", function () {
-  modal.style.display = "block";
-  body.style.overflow = "hidden";
-});
-
-window.addEventListener("click", function (e) {
-  if (e.target === modal) {
-    modal.style.display = "none";
-    body.style.overflow = "auto";
+class library {
+  constructor() {
+    this.library = [];
   }
-});
 
-function submitBook() {
-  const title = document.querySelector("#modalTitle").value;
-  const author = document.querySelector("#modalAuthor").value;
-  const page = document.querySelector("#modalPage").value;
-  const readCheck = document.querySelector("#modalCheck");
-  const displayBooks = document.querySelector(".displayBooks");
+  submitBook() {
+    const add = document.querySelector(".add");
+    const modal = document.querySelector(".modal");
+    const modalContent = document.querySelector(".modal-content");
+    const addBook = document.querySelector("#addBook");
+    const displayBooks = document.querySelector(".displayBooks");
 
-  const createDiv = document.createElement("div");
-  createDiv.className = "books";
-  if (readCheck.checked) {
-    createDiv.innerHTML = `<h3>${title}</h3>
-   <p>Author:${author}</p>
-   <p>Page:${page}</p>
-   <p>read<p>
-  `;
-  } else {
-    createDiv.innerHTML = `<h3>${title}</h3>
-  <p>Author:${author}</p>
-  <p>Page:${page}</p>
-  <p>unread</p>
- `;
+    add.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
+    window.addEventListener("click", (e) => {
+      if (e.target !== add && !modalContent.contains(e.target)) {
+        modal.style.display = "none";
+      }
+    });
+
+    addBook.addEventListener("click", () => {
+      let title = document.querySelector("#modalTitle");
+      let author = document.querySelector("#modalAuthor");
+      let page = document.querySelector("#modalPage");
+      let readCheck = document.querySelector("#modalCheck");
+      let book = {
+        title: title.value,
+        author: author.value,
+        page: page.value,
+        read: readCheck.checked,
+      };
+      this.library.push(book);
+      modal.style.display = "none";
+      title.value = "";
+      author.value = "";
+      page.value = "";
+      readCheck.checked = false;
+
+      displayBooks.innerHTML = "";
+      this.library.forEach((book) => {
+        const bookDiv = document.createElement("div");
+        bookDiv.classList.add("book");
+        bookDiv.innerHTML = `
+        <h3>Title:${book.title}</h3>
+        <p>Author:${book.author}</p>
+        <p>page:${book.page}</p>
+        <p> ${book.read ? "read" : "not read"} </p>`;
+        displayBooks.appendChild(bookDiv);
+      });
+    });
   }
-  displayBooks.appendChild(createDiv);
-  modal.style.display = "none";
-  body.style.overflow = "auto";
-  document.querySelector("#modalTitle").value = "";
-  document.querySelector("#modalAuthor").value = "";
-  document.querySelector("#modalPage").value = "";
-  readCheck.checked = false;
 }
+
+const myLibrary = new library();
+
+myLibrary.submitBook();
